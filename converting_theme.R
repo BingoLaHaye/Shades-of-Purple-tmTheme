a80641c2706e7c0266d17f78b2eaa4ddca10cd19
Bingo LaHaye <bingolahaye@msn.com>
2019-11-27 15:12:16 -0500

    changed parenthesis to a light pink

--- converting_theme.R
+++ converting_theme.R
@@ -1,5 +1,5 @@
 library(rstudioapi)
 # convertTheme("Shades-of-Purple-tmTheme-master/Shades-of-Purple-tmTheme-master/Shades-of-Purple.tmTheme", add = FALSE,
             # outputLocation = "C:/Users/bingo/OneDrive/CUA/R/ShadesOfPurple_Rstudio", force = TRUE)
-rstudioapi::addTheme("Shades-of-Purple.rstheme", apply = TRUE, force = TRUE)
+rstudioapi::addTheme("C:/Users/bingo/OneDrive/CUA/R/Shades-of-Purple-tmTheme/Shades-of-Purple.rstheme", apply = TRUE, force = TRUE)
 rstudioapi::removeTheme("Shades-of-Purple")
